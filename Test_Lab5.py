88743c9f3dd4558faff5a4ccc1de1e4296b9c498
Caitlinlaplante <caitlaplante@icloud.com>
2026-03-26 20:10:33 -0400

    Update Test_Lab5.py

--- Test_Lab5.py
+++ Test_Lab5.py
@@ -9,38 +9,58 @@ class TestNumbers(unittest.TestCase):
         self.number = Numbers(10)
     #4.1
     def test_factorial(self):
+
+        #Right result of factorial
         self.assertEqual(self.number.factorial(4), 24, 'The factorial is right.')
+
+        #Wrong result of factorial
         self.assertNotEqual(self.number.factorial(6), 700, 'The factorial is wrong.')
-        self.assertEqual(self.number.factorial(1), 1, 'Negative not allowed.')
+
+        #Negative factorial
+        with self.assertRaises(ValueError):
+            self.number.factorial(-1)
 
     #4.2
     def test_addToSum(self):
+        #Add value
         self.number.addToSum(5)
         self.assertEqual(self.number.sum, 15, "Sum should increase by 5")
-    
+
     def testmultiple_addToSum(self):
+        #Add multiple value
         self.number.addToSum(3)
         self.number.addToSum(2)
         self.assertEqual(self.number.sum, 15, "Sum should accumulate add 5")
 
     def test_subtractFromSm(self):
+        #Subtract value
         self.number.subtractFromSum(4)
         self.assertEqual(self.number.sum, 6, "Sum should decrease by 4")
     
     def testmultiple_subtractFromSum(self):
+        #Subtract multiple value
         self.number.subtractFromSum(3)
         self.number.subtractFromSum(2)
         self.assertEqual(self.number.sum, 5, "Sum should accumulate subtract 5")
     
     def test_add_and_subtract(self):
+        #Mix of adding and subtracting
         self.number.addToSum(5)
         self.number.subtractFromSum(3)
-        self.assertEqual(self.number.sum, 12, "Combined operations should work")
+        self.assertEqual(self.number.sum, 12, "Combined operations works")
 
     #4.3
     def test_stringOfNumber(self):
+        #String of a number
         self.assertEqual(self.number.stringOfNumber(5), "five", "the string is 5")
-        self.assertNotEqual(self.number.stringOfNumber(5), "four", "the string is 5")
+        #String with the wrong number
+        self.assertNotEqual(self.number.stringOfNumber(7), "four", "the string is 5")
+        #Type Error
+        with self.assertRaises(TypeError):
+            self.number.stringOfNumber(5.0)
+        #Value Error
+        with self.assertRaises(ValueError):
+            self.number.stringOfNumber(10)
 
 #5
 class TestECG(unittest.TestCase):
@@ -49,25 +69,34 @@ class TestECG(unittest.TestCase):
         self.ecg = ECG()
     #5.1
     def test_detect_peaks(self):
-        peaks = self.ecg.detect_peaks(self.test_signal, 0.5)
-        self.assertEqual(peaks, [2,5],"Should detect peaks at 2 and 5")
-
-    def test_detect_no_peaks(self):
-        signal = [0.1, 0.2, 0.3, 0.4]
-        peaks = self.ecg.detect_peaks(signal, 0.1)
-        self.assertEqual(peaks, [], "Should detect no peaks")
+        #More than one peaks
+        self.assertEqual(self.ecg.detect_peaks(self.test_signal, 0.5), [2,5],"Detect peaks at 2 and 5")
+        #No peaks
+        self.assertEqual(self.ecg.detect_peaks([0.1, 0.2, 0.3, 0.4], 0.1), [], "Detect no peaks")
+        #One peak
+        self.assertEqual(self.ecg.detect_peaks([0.1,0.6,0.1], 0.5), [1], "Dectect one peak at 1")
+        #Less than 3 values
+        self.assertEqual(self.ecg.detect_peaks([0.1,0.6], 0.5), [], "Dectect one peak at 1")
 
     #5.2
     def test_remove_baseline(self):
+        #Baseline removed from signal
         result = self.ecg.remove_baseline(self.test_signal)
         expected = [-0.41428571428571437,-0.31428571428571433,0.6857142857142856,-0.21428571428571436,-0.41428571428571437,0.9857142857142857,-0.31428571428571433]
         self.assertEqual(result, expected)
+        #Value Error when signal empty
+        with self.assertRaises(ValueError):
+            self.ecg.remove_baseline([])
 
     #5.3
     def test_normalize(self):
+        #Normalized signal
         result = self.ecg.normalize(self.test_signal)
         expected = [0.06666666666666667,0.13333333333333333,0.7999999999999999,0.19999999999999998,0.06666666666666667,1.0,0.13333333333333333]
         self.assertEqual(result, expected)
+        #Value Error when signal is empty
+        with self.assertRaises(ValueError):
+            self.ecg.normalize([])
 
     #6
     #6.1
